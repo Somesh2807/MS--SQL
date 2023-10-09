697e5f0ccab632a92231d2ab8e0cde36712d0772
Somesh Jatav <110923806+Somesh2807@users.noreply.github.com>
2023-10-09 23:01:57 +0530

    commit

--- Advanced SQL/Test.sql
+++ Advanced SQL/Test.sql
@@ -3,3 +3,21 @@ create database windows_function
 use windows_function
 
 
+--- Windows Function
+
+select * from rise.dbo.employee
+
+
+UPDATE rise.dbo.employee 
+SET Gender = 'FeMale' 
+WHERE id BETWEEN 6 AND 7;
+
+select first_name,last_name,Gender,
+count(Gender) over (partition  by Gender)  as Gender_total
+,min(salary) over (partition by gender) as Min_salary
+,max(salary) over (partition by gender) as max_Salary
+from rise.dbo.employee
+order by first_name asc
+
+
+
