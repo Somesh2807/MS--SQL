e85aa617e8ff3100d0c76cfa2d0eea1e9b088ffb
Somesh Jatav <110923806+Somesh2807@users.noreply.github.com>
2023-10-09 22:38:19 +0530

    Update RSPL_1108.sql

--- RSPL_1108.sql
+++ RSPL_1108.sql
@@ -924,4 +924,9 @@ from B_Films B
 join H_Films H on b.Length = h.Length
 
 -- Written a query for a particular customer who booked ticket on particular date.
+
 use rise
+
+select b.Film_Name as Bollywood_Name,h.Film_Name as Hollywod_Name,b.Length as Movie_Length
+from B_Films B
+join H_Films H on b.Length = h.Length
